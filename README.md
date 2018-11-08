4b421ef8f7c516ebcee85bbfa92433e42655766b
Patrick Springstubbe <pat@springstubbe.us>
2018-11-08 14:17:10 -0500

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# jQuery-DragNDrop
+# jQuery DragNDrop
 Enable drag and drop functionality on a list of elements.
+
+## Support
+I only provide limited support through the Github Issues area.  DO NOT ask for support via email, socialmedia, or other means.  Also check the closed issues before opening a new issue.
+
+## Design Methodology
+This plugin is not meant to be a full featured drag & drop plugin.  I envision this as a plugin that contains a base set of features that can be enhanced using callbacks by the developer.  Because of this I am hesitant on adding functionality but am open to adding callbacks where it makes sense.  But feel free to open an [issue](https://github.com/nobleclem/jQuery-DragNDrop/issues) and suggest a feature request if you feel that most people will benefit from it.
+
+## Demo
+http://springstubbe.us/projects/jquery-dragndrop/
+
+## Usage
+```
+$('ul').dragndrop();
+$('ul').dragndrop('unload');
+$('ul').dragndrop('reload');
+$('ul').dragndrop({
+    onDrop: function( element, droppedElement ){}
+});
+```
+
+### Options
+
+
+### Methods
+**unload**
+Disable the jQuery Drag & Drop functionality.
+
+**reload**
+Reload the jQuery Drag & Drop functionality.  Useful if you dynamically add/remove items to be dragged and dropped.
+
+**reset**
+Reset elements back to their original position when page loaded.
+NOT YET IMPLEMENTED
+
+
+### Callbacks
+**onDrop**
+
+Fires after element is dropped into its new position.
+
+`onDrop( element, droppedElement )`
+
+element: The original element targeted for .dragndrop().
+
+droppedElement: The element that was moved.
---
+++ jquery.dragndrop.js
@@ -0,0 +1,164 @@
+/**
+ * Simple Drag & Drop
+ * @Version: 1.0
+ * @Author: Patrick Springstubbe
+ * @Contact: @JediNobleclem
+ * @Website: springstubbe.us
+ * @Source: https://github.com/nobleclem/jQuery-DragNDrop
+ *
+ * Usage:
+ *     $('ul').dragndrop();
+ *     $('ul').dragndrop('unload');
+ *     $('ul').dragndrop('reload');
+ *     $('ul').dragndrop({
+ *         onDrop: function( element, droppedElement ){}
+ *     });
+ *
+ **/
+(function($){
+    var defaults = {
+        loadStyles: true, // load default Drag & Drop Styles
+
+        // Callbacks
+        onDrop: function( element, droppedElement ) {}
+    };
+
+    var stylesLoaded = false;
+    var stylesDefaults = $('<style type="text/css">.jqdndLoaded > * { cursor: move; user-select: none; } .jqdndLoaded .jqdndDragging { opacity: 0.5; }</style>');
+
+    /* Add Plugin to jQuery */
+    $.fn.dragndrop = function( options ) {
+        if( !this.length ) {
+            return;
+        }
+
+        var args = arguments;
+        var ret;
+
+        // initialize drag and drop on element(s)
+        if( (options === undefined) || (typeof options === 'object' ) ) {
+            return this.each(function(){
+                if( !$.data( this, 'plugin_dragndrop' ) ) {
+                    $.data( this, 'plugin_dragndrop', new DragNDrop( this, options ) );
+                }
+            });
+        // execute built in method (unload, reload, etc) on element(s)
+        } else if( (typeof options === 'string') && (options[0] !== '_') && (options !== 'init') ) {
+            this.each(function(){
+                var instance = $.data( this, 'plugin_dragndrop' );
+
+                if( (instance instanceof DragNDrop) && (typeof instance[ options ] === 'function') ) {
+                    ret = instance[ options ].apply( instance, Array.prototype.slice.call( args, 1 ) );
+                }
+            });
+
+            return ret;
+        }
+    };
+
+
+    /* Setup Functionality */
+    function DragNDrop( element, options )
+    {
+        this.element   = element;
+        this.options   = $.extend( true, {}, defaults, options );
+        this.dragElem  = null;
+
+        if( !stylesLoaded ) {
+            stylesLoaded = true;
+
+            if( $('head > link:eq(0)').length ) {
+                stylesDefaults.insertBefore( $('head > link:eq(0)') );
+            }
+            else if( $('head > style:eq(0)').length ) {
+                stylesDefaults.insertBefore( $('head > style:eq(0)') );
+            }
+            else {
+                $('head').append( stylesDefaults );
+            }
+        }
+
+        this.load();
+    }
+
+    DragNDrop.prototype = {
+        load: function() {
+            var instance = this;
+            var items    = $(instance.element).find('> *');
+
+            // make sure this has not already been loaded
+            if( $(instance.element).data( 'plugin_dragndrop' ) ) {
+                return true;
+            }
+
+            $(instance.element).addClass('jqdndLoaded');
+
+            items.attr('draggable', true ).each(function(){
+                $(this)
+                    .on( 'dragstart', function( event ){
+                        instance.dragElem = this;
+
+                        event.originalEvent.dataTransfer.effectAllowed = 'move';
+                        $(this).addClass('jqdndDragging');
+                    })
+                    .on( 'dragenter', function( event ){})
+                    .on( 'dragover', function( event ){
+                        event.preventDefault();
+                        event.originalEvent.dataTransfer.dropEffect = 'move';
+
+                        if( this == instance.dragElem ) {
+                            return;
+                        }
+                        else if( $(this).prevAll().filter( instance.dragElem ).length ) {
+                            $(instance.dragElem).insertAfter( $(this) );
+                        }
+                        else {
+                            $(instance.dragElem).insertBefore( $(this) );
+                        }
+
+                        return false;
+                    })
+                    .on( 'dragleave', function( event ){})
+                    .on( 'drop', function( event ){
+                        event.preventDefault();
+
+                        if( typeof instance.options.onDrop == 'function' ) {
+                            instance.options.onDrop( instance.element, instance.dragElem );
+                        }
+                    })
+                    .on( 'dragend', function( event ){
+                        event.preventDefault();
+                        $(this).removeClass('jqdndDragging');
+
+                        instance.dragElem = null;
+                    })
+            });
+        },
+
+        unload: function(){
+            var instance = this;
+            var items    = $(instance.element).find('> *');
+
+            $(instance.element).removeClass('jqdndLoaded');
+
+            items.removeAttr('draggable').each(function(){
+                $(this)
+                    .off( 'dragstart' )
+                    .off( 'dragenter' )
+                    .off( 'dragover' )
+                    .off( 'dragleave' )
+                    .off( 'drop' )
+                    .off( 'dragend' );
+            });
+
+            $(instance.element).data( 'plugin_dragndrop', null );
+        },
+
+        reload: function() {
+            this.unload();
+            this.load();
+        },
+
+        reset: function(){}
+    };
+}(jQuery));
